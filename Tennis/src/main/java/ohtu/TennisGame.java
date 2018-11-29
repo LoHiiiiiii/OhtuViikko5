27df4911257f3a6239fbbcc2cc6af0115abce5cd
Vertti Viitanen <vertti.viitanen@gmail.com>
2018-11-29 20:26:46 +0200

    - Task 2

--- Tennis/src/main/java/ohtu/TennisGame.java
+++ Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,11 +1,12 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
+    int pointsToWin = 4; 
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,68 +14,60 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals(player1Name)) {
+            player1Score += 1;
+        } else {
+            player2Score += 1;
+        }
+    }
+
+    private String pointToText(int points) {
+        switch (points) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        return "";
+    }
+
+    private String tieUnderWinPoints() {
+        return pointToText(player1Score) + "-All";
+    }
+
+    private String playerAtOrOverWinPoints() {
+        if (player1Score == player2Score) {
+            return "Deuce";
+        }
+        if (player1Score > player2Score)
+            return advantageOrWin(player1Name);
         else
-            m_score2 += 1;
+            return advantageOrWin(player2Name);
+    }
+    
+    private String advantageOrWin(String player) {
+        if (Math.abs(player1Score-player2Score) == 1){
+            return "Advantage " + player;
+        } else {
+            return "Win for " + player;
+        }
+    }
+    
+    private String differenceUnderWinPoints(){
+        return pointToText(player1Score) + "-" + pointToText(player2Score);
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (player1Score >= pointsToWin || player2Score >= pointsToWin) {
+            return playerAtOrOverWinPoints();
+        } else if (player1Score == player2Score) {
+            return tieUnderWinPoints();
+        } else {
+            return differenceUnderWinPoints();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
     }
 }
